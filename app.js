2b91a51c2b5e2299e9692afd53e3175580e571d0
delcio-torres <delcio.at@gmail.com>
2024-01-26 23:00:50 -0300

    Inclusão de letras maúsculas e acentuação

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-let letra = [ ' ','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+let letra = [' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+  'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'á', 'é', 'í', 'ó', 'ú', 'ê', 'ô'];
 
-let cript = ['6', 'l','k','j','h','g','n','d','s','8','p','o','3','4','y','t','r','0','w','q','m','f','b','v','c','x','z']
+let cript = ['6', 'F', 'ê', 'W', '9', 'l', 'k', 'Q', 'j', 'h', 'J', 'é', 'X', 'g', 'í', 'E', 'K', 'n', 'B', 'd', 'I', 's', 'ó', 'C', '8', 'Y', 'p', 'H', 'o', 'G', '3', 'V', 'S', 'R', '4', 'T', 'U', 'y', 'D', 't', 'r',
+  'Z', '0', 'L', 'w', 'q', 'P', 'á', 'm', 'M', 'f', 'A', 'b', 'v', 'N', 'O', 'c', 'ú', 'x', 'z']
 
 let digito;
 let palavraCodificada = '';
@@ -13,7 +15,7 @@ function criptografar() {
   palavra = texto1.value;
   for (let i = 0; i <= palavra.length; i++) {
     for (let y = 0; y <= cript.length; y++) {
-      digito = palavra.substring(i, i + 1)
+      digito = palavra.substring(i, i + 1);
       if (digito == letra[y]) {
         palavraCodificada += cript[y];    
       }
@@ -28,7 +30,7 @@ function decriptografar() {
   let palavra2 = texto2.value;
   for (let i = 0; i <= palavra2.length; i++) {
     for (let y = 0; y <= cript.length; y++) {
-      digito = palavra2.substring(i, i + 1)
+      digito = palavra2.substring(i, i + 1);
       if (digito == cript[y]) {
         palavraDecodificada += letra[y];    
       }
